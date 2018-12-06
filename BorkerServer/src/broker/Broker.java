9f012555bbb37863b9da788f3c974a2a92fa3071
fez3d <soydanielmedina@gmail.com>
2018-12-06 15:04:04 -0600

    Todas las paginas tienen el mismo nav

--- BorkerServer/src/broker/Broker.java
+++ BorkerServer/src/broker/Broker.java
@@ -5,14 +5,29 @@
  */
 package broker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author dmc-4
  */
 public class Broker {
     
-    public void mainEventLoop(){
-        
+    public void mainEventLoop() throws IOException{
+        ServerSocket socketServer = new ServerSocket(1234);
+        ServerSocket socketClient = new ServerSocket(1235);
+        while(true){
+            if(findClient(socketClient)){
+                
+            }
+        }
     }
     
     public void updateRepository(){
@@ -23,8 +38,22 @@ public class Broker {
         
     }
     
-    public void findClient(){
-        
+    public boolean findClient(ServerSocket socketClient){
+        try {
+            Socket fromClientSocket = socketClient.accept();
+            PrintWriter pw = new PrintWriter(fromClientSocket.getOutputStream(), true);
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(fromClientSocket.getInputStream()));
+            String str;
+            while ((str = br.readLine()) != null) {
+                System.out.println("The message: " + str);
+                
+            }
+            
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
     }
     
     public void fowardRequest(){
--- BorkerServer/src/broker/ClienteProxy.java
+++ BorkerServer/src/broker/ClienteProxy.java
@@ -5,6 +5,11 @@
  */
 package broker;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author dmc-4
@@ -19,8 +24,19 @@ public class ClienteProxy {
         
     }
     
-    public void sendRequest(){
-        
+    public void sendRequest() throws IOException{
+        Socket socket = null;
+        OutputStreamWriter osw;
+        String str = "Hello World";
+        try {
+            socket = new Socket("localhost", 1235);
+            osw =new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+            osw.write(str, 0, str.length());
+        } catch (IOException e) {
+            System.err.print(e);
+        } finally {
+            socket.close();
+        }
     }
     
     public void regresar(){
